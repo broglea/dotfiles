e2d7feb9ecb084785482499adedf86c1bc828869
broglea <austin.brogle@ucf.edu>
2014-10-08 00:32:25 -0400

    Created install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+if uname | grep -q Darwin
+then
+  mv -f ~/.vimrc ~/.vimrc.old;
+  cp -f ./.vimrc_mac ~/.vimrc;
+  mv -f ~/.zshrc ~/.zshrc.old;
+  cp -f ./.zshrc_mac ~/.zshrc;
+fi
+
+if uname | grep -q Linux
+then
+  mv -f ~/.vimrc ~/.vimrc.old;
+  cp -f ./.vimrc_linux ~/.vimrc;
+  mv -f ~/.zshrc ~/.zshrc.old;
+  cp -f ./.zshrc_linux ~/.zshrc;
+fi
+
+# zsh config
+curl -L http://install.ohmyz.sh | sh
+source ~/.zshrc
+
+# vim config
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
